ff22596e81999ec99a41a7d453bd2364ac14aed0
2jun0 <soo28819@naver.com>
2022-08-20 00:50:04 +0900

    add: init java spring boot server

---
+++ src/main/java/ejun0/ytcaptionchecker/server/yt/caption/checker/server/YtCaptionCheckerServerApplication.java
@@ -0,0 +1,13 @@
+package ejun0.ytcaptionchecker.server.yt.caption.checker.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtCaptionCheckerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtCaptionCheckerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ejun0/ytcaptionchecker/server/yt/caption/checker/server/YtCaptionCheckerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ejun0.ytcaptionchecker.server.yt.caption.checker.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtCaptionCheckerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
